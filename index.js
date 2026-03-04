47d3d88c7c476765569796bf2f1f78da252d9066
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-04 23:00:52 +0600

    error handle completed

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const displayLevelWord = (words) => {
   // 1. Get the Container and empty
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+      <div class="bg-slate-200 col-span-full py-16 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+    `;
+  }
   //   2. Get into every word
   for (const word of words) {
     // 3.Create element
@@ -23,7 +33,9 @@ const displayLevelWord = (words) => {
         <div class="bg-white space-y-4 rounded py-8">
           <h2 class="text-3xl font-bold">${word.word}</h2>
           <p class="text-xl font-medium"> Meaning / Pronunciation</p>
-          <h2 class="font-bangla font-semibold text-3xl">"${word.meaning} / ${word.pronunciation}"</h2>
+          <h2 class="font-bangla font-semibold text-3xl">"${
+            word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি"
+          } / ${word.pronunciation}"</h2>
           <div class="flex justify-between px-12">
             <button class="btn bg-[#1A91FF20]">
               <i class="fa-solid fa-circle-info"></i>
@@ -32,6 +44,7 @@ const displayLevelWord = (words) => {
           </div>
         </div>
     `;
+    // 4.Append element
     wordContainer.append(card);
   }
 };
